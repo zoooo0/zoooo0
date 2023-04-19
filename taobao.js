ca29aedb804ec7cb78d987f017171ee69b3b3c98
zoooo0 <62314432+zoooo0@users.noreply.github.com>
2023-04-20 07:34:05 +0800

    Update taobao.js

--- taobao.js
+++ taobao.js
@@ -4,5 +4,5 @@ const pattern = /^https:\/\/h5api\.m\.taobao\.com\/h5\/mtop\./;
 if (pattern.test(url)) {
   const cookie = $request.headers['Cookie'];
   $persistentStore.write(cookie, 'taobao_cookie');
-  console.log('Successfully saved Taobao cookie 🎉');
+  console.log(`Successfully saved Taobao cookie: ${cookie}`);
 }
