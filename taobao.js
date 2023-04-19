da5bfb2e94b8fa825f7eb40576d865aab95c3f4c
zoooo0 <62314432+zoooo0@users.noreply.github.com>
2023-04-20 07:30:22 +0800

    Create taobao.js

---
+++ taobao.js
@@ -0,0 +1,8 @@
+const url = $request.url;
+const pattern = /^https:\/\/h5api\.m\.taobao\.com\/h5\/mtop\./;
+
+if (pattern.test(url)) {
+  const cookie = $request.headers['Cookie'];
+  $persistentStore.write(cookie, 'taobao_cookie');
+  console.log('Successfully saved Taobao cookie 🎉');
+}
