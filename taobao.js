f467b750c53ffdd28ef759e615762c533489055a
zoooo0 <62314432+zoooo0@users.noreply.github.com>
2025-09-24 15:18:55 +0800

    Delete taobao.js

--- taobao.js
+++
@@ -1,8 +0,0 @@
-const url = $request.url;
-const pattern = /^https:\/\/h5api\.m\.taobao\.com\/h5\/mtop\./;
-
-if (pattern.test(url)) {
-  const cookie = $request.headers['Cookie'];
-  $persistentStore.write(cookie, 'taobao_cookie');
-  console.log(`Successfully saved Taobao cookie: ${cookie}`);
-}
